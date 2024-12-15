3011629fbf10b82bfd1b97441c057aea1d2c3a0c
Consolata <consolatawanjiru45@gmail.com>
2024-12-15 21:45:28 +0300

    Fix bug

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.generics import ListAPIView, CreateAPIView
+from .models import Item
+from .serializers import ItemSerializer
+
+class ListItemsView(ListAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+class CreateItemView(CreateAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
