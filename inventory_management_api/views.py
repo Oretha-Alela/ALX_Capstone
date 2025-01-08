56d6ecd3665458d902fa0a507b8f27fc1dd1649a
Consolata <consolatawanjiru45@gmail.com>
2025-01-08 18:14:34 +0300

    My project

--- inventory_management_api/views.py
+++ inventory_management_api/views.py
@@ -1,11 +1,22 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
+from rest_framework.response import Response
 from .models import InventoryItem, InventoryChangeLog
 from .serializers import InventoryItemSerializer, InventoryChangeLogSerializer
+from .permissions import IsOwnerPermission
 
+
+
+
+# List and Create Inventory Items
 class InventoryItemListCreateView(generics.ListCreateAPIView):
     serializer_class = InventoryItemSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ['category', 'price']
+    ordering_fields = ['name', 'quantity', 'price', 'date_added']
 
     def get_queryset(self):
         return InventoryItem.objects.filter(user=self.request.user)
@@ -13,17 +24,18 @@ class InventoryItemListCreateView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
+# Retrieve, Update, and Delete Inventory Items
 class InventoryItemDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = InventoryItem.objects.all()
     serializer_class = InventoryItemSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwnerPermission]
 
-    def get_queryset(self):
-        return InventoryItem.objects.filter(user=self.request.user)
-
+# Inventory Change History
 class InventoryChangeLogView(generics.ListAPIView):
     serializer_class = InventoryChangeLogSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        item_id = self.kwargs['item_id']
-        return InventoryChangeLog.objects.filter(item__id=item_id, item__user=self.request.user)
+        item_id = self.kwargs.get('item_id')
+        return InventoryChangeLog.objects.filter(item_id=item_id)
+
