62695e4cb2a23aca618160ef4cd2ba2d753b677f
Consolata <consolatawanjiru45@gmail.com>
2025-01-11 18:59:54 +0300

    My Project

--- inventory/urls.py
+++ inventory/urls.py
@@ -14,11 +14,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+from django.http import HttpResponse
+
+#def home_view(request):
+    #return HttpResponse("Welcome to the Inventory Management API!")
+
+def redirect_to_api(request):
+    return redirect('/api/')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('inventory_management_api.urls')),
+    path('', redirect_to_api),
+
+    #path('', home_view, name='home'),  
 ]
+
 
