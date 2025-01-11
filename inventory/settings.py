4440bae7505f2b2d06c6a7800aab8b2136d970d1
Consolata <consolatawanjiru45@gmail.com>
2025-01-11 18:44:43 +0300

    My Project

--- inventory/settings.py
+++ inventory/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-896f4b8fo$e##p9c2u6ijl_%-%@_+dab6h+$jyhizc244l)0no'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 
