4970bbe2671c2c5a8c191f8bd7b0115a7a6a3412
Consolata <consolatawanjiru45@gmail.com>
2025-01-11 14:23:48 +0300

    My Project

--- inventory_management_api/urls.py
+++ inventory_management_api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import InventoryView
+# from .views import InventoryView
 from django.contrib.auth import views as auth_views
 from .views import InventoryItemListCreateView, InventoryItemDetailView, InventoryChangeLogView, InventoryItemListView
 
@@ -9,7 +9,7 @@ urlpatterns = [
     path('inventory/<int:item_id>/logs/', InventoryChangeLogView.as_view(), name='inventory-change-logs'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('inventory/', InventoryView.as_view(), name='inventory'),
+    # path('inventory/', InventoryView.as_view(), name='inventory'),
     path('inventory/', InventoryItemListView.as_view(), name='inventory-list'),
 ]
 
