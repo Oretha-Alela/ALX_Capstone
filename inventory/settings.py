e2a5c8cfb023bbff2e32593c02c5035e2a10dfc2
Consolata <consolatawanjiru45@gmail.com>
2025-01-11 14:48:45 +0300

    My Project

--- inventory/settings.py
+++ inventory/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-896f4b8fo$e##p9c2u6ijl_%-%@_+dab6h+$jyhizc244l)0no
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 
